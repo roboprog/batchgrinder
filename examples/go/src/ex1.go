a8aff239190025eba03fe0da8be12ab4b23f524c
Rob Anderson <roboprog@yahoo.com>
2013-05-10 22:07:00 -0700

    first cut at Go language directory layout

---
+++ examples/go/src/ex1.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"./com/roboprogs/batchgrinder/grinder"
+)
+
+// TODO:  process something
+func main(
+		) {
+	grinder.Run()
+}
+
+
+// vi: ts=4 sw=4
+// *** EOF ****
---
+++ libs/go/src/com/roboprogs/batchgrinder/grinder/grinder.go
@@ -0,0 +1,17 @@
+package grinder
+
+import (
+	"log"
+)
+
+// TODO:  process something
+func Run(
+		) {
+	log.SetFlags( log.Ldate | log.Lmicroseconds | log.Lshortfile)
+	log.Printf( "Get started!\n")
+	log.Fatalf( "THE END\n")
+}
+
+
+// vi: ts=4 sw=4
+// *** EOF ****
