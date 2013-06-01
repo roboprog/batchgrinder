5cb1d55ea5a2ef4c97aae612811fbf962a2ee96d
Rob Anderson <roboprog@yahoo.com>
2013-05-31 22:43:32 -0700

    simulate a job that reads and writes lines of text

--- examples/go/src/ex1.go
+++ examples/go/src/ex1.go
@@ -1,16 +1,46 @@
 package main
 
 import (
+	"fmt"
+
 	"./com/roboprogs/batchgrinder/grinder"
 )
 
-// TODO:  process something
+// simulate a batch job, using framework to pipeline steps
 func main(
 		) {
+
+	// pretend to read input
+	load := func(
+			num int) interface {} {
+		if num > 6 {
+			return nil
+		}
+
+		in_rec := fmt.Sprintf( "In rec %d", num)
+		return in_rec
+	}
+
+	// pretend to process input and create output
+	transform := func (
+			in_data interface {},
+			num int) interface {} {
+		in_rec := in_data.( string)
+		return in_rec + " X"
+	}
+
+	// pretend to write output
+	dump := func (
+			out_data interface {},
+			num int) {
+		out_rec := out_data.( string)
+		fmt.Printf( "%d) %s\n", num, out_rec)
+	}
+
 	grinder.Run(
-			grinder.Loader {},
-			grinder.Transformer {},
-			grinder.Dumper {})
+			grinder.Loader { &load, nil, nil },
+			grinder.Transformer { &transform, nil, nil },
+			grinder.Dumper { &dump, nil, nil })
 }
 
 
