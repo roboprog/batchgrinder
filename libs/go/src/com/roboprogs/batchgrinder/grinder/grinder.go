2a7ba619945dd834aa458e8c34a6f4bec8ac1c0e
Rob Anderson <roboprog@yahoo.com>
2013-05-31 22:42:51 -0700

    deal with EOF detection, and get rid of surplus stars on thars

--- libs/go/src/com/roboprogs/batchgrinder/grinder/grinder.go
+++ libs/go/src/com/roboprogs/batchgrinder/grinder/grinder.go
@@ -13,37 +13,37 @@ type (
 	// nor do the routines even need to be actual methods.
 	Loader struct {
 		// callback to get the main processing units
-		Unit * func ( int) * interface {}
+		Unit * func ( int) interface {}
 
 		// callback to get the [file] header
-		Header * func () * interface {}
+		Header * func () interface {}
 
 		// callback to get the [file] trailer
-		Trailer * func () * interface {}
+		Trailer * func () interface {}
 	}
 
 	// function pointers to validate and transform data for a job
 	Transformer struct {
 		// callback to transform / validate the main processing units
-		Unit * func ( * interface {}, int) * interface {}
+		Unit * func ( interface {}, int) interface {}
 
 		// callback to transform / validate the [file] header
-		Header * func ( * interface {}) * interface {}
+		Header * func ( interface {}) interface {}
 
 		// callback to transform / validate the [file] trailer
-		Trailer * func ( * interface {}) * interface {}
+		Trailer * func ( interface {}) interface {}
 	}
 
 	// function pointers to create output for a job
 	Dumper struct {
 		// callback to put the main processing units
-		Unit * func ( * interface {}, int)
+		Unit * func ( interface {}, int)
 
 		// callback to put the [file] header
-		Header * func ( * interface {})
+		Header * func ( interface {})
 
 		// callback to put the [file] trailer
-		Trailer * func ( * interface {})
+		Trailer * func ( interface {})
 	}
 )
 
@@ -55,6 +55,8 @@ func Run(
 		dump Dumper) {
 	log.SetFlags( log.Ldate | log.Lmicroseconds | log.Lshortfile)  // TODO:  external config
 	log.Printf( "BEGIN\n")
+
+	// TODO:  set number of CPUs available for program
 
 	proc_hdr( load, transform, dump)
 
@@ -70,7 +72,7 @@ func proc_hdr(
 		load Loader,
 		transform Transformer,
 		dump Dumper) {
-	in_hdr := func () * interface {} {
+	in_hdr := func () interface {} {
 		if load.Header != nil {
 			log.Printf( "Read header\n")
 			return ( *load.Header)()
@@ -78,7 +80,7 @@ func proc_hdr(
 		return nil
 	} ()
 
-	out_hdr := func () * interface {} {
+	out_hdr := func () interface {} {
 		if transform.Header != nil {
 			log.Printf( "Process header\n")
 			return ( *transform.Header)( in_hdr)
@@ -97,44 +99,53 @@ func proc_units(
 		load Loader,
 		transform Transformer,
 		dump Dumper) {
-	loaded_units := make( chan * interface {})
-	transformed_units := make( chan * interface {})
+	loaded_units := make( chan interface {})
+	transformed_units := make( chan interface {})
+	eof := make( chan string)
 
 	go load_units( load, loaded_units)
 	go transform_units( transform, loaded_units, transformed_units)
-	go dump_units( dump, transformed_units)
-	// TODO:  wait for EOF, continue
+	go dump_units( dump, transformed_units, eof)
+	<- eof
 }
 
 // load "units" (customers, statements, messages, whatever)
 func load_units(
 		load Loader,
-		loaded_units chan * interface {}) {
-	log.Printf( "Read units\n")
+		loaded_units chan interface {}) {
 	num := 0
 	for {
 		num++
-		// TODO: periodic progress message
 		in_unit := ( *load.Unit)( num)
-		// TODO: check for errors / EOF
+		// TODO: check for errors
 		loaded_units <- in_unit
+		if in_unit == nil {
+			return
+		}
+
+		log.Printf( "Read unit %d\n", num)
+		// TODO: periodic progress message
 	}
 }
 
 // transform "units" (customers, statements, messages, whatever)
 func transform_units(
 		transform Transformer,
-		loaded_units chan * interface {},
-		transformed_units chan * interface {}) {
-	log.Printf( "Process units\n")
+		loaded_units chan interface {},
+		transformed_units chan interface {}) {
 	num := 0
 	for {
 		num++
-		// TODO: periodic progress message
 		in_unit := ( <- loaded_units)
-		// TODO: EOF
+		if in_unit == nil {
+			transformed_units <- nil
+			return
+		}
+
 		out_unit := ( *transform.Unit)( in_unit, num)
 		// TODO: check for errors
+		log.Printf( "Processed unit %d\n", num)
+		// TODO: periodic progress message
 		transformed_units <- out_unit
 	}
 }
@@ -142,16 +153,20 @@ func transform_units(
 // dump "units" (customers, statements, messages, whatever)
 func dump_units(
 		dump Dumper,
-		transformed_units chan * interface {}) {
-	log.Printf( "Write units\n")
+		transformed_units chan interface {},
+		eof chan string) {
 	num := 0
 	for {
 		num++
-		// TODO: periodic progress message
 		out_unit := ( <- transformed_units)
-		// TODO: EOF
-		( *dump.Unit)( out_unit, num)  // TODO:  counter
+		if out_unit == nil {
+			eof <- "EOF"
+			return
+		}
+		( *dump.Unit)( out_unit, num)
 		// TODO: check for errors
+		log.Printf( "Wrote unit %d\n", num)
+		// TODO: periodic progress message
 	}
 }
 
@@ -160,7 +175,7 @@ func proc_tlr(
 		load Loader,
 		transform Transformer,
 		dump Dumper) {
-	in_tlr := func () * interface {} {
+	in_tlr := func () interface {} {
 		if load.Trailer != nil {
 			log.Printf( "Read trailer\n")
 			return ( *load.Trailer)()
@@ -168,7 +183,7 @@ func proc_tlr(
 		return nil
 	} ()
 
-	out_tlr := func () * interface {} {
+	out_tlr := func () interface {} {
 		if transform.Trailer != nil {
 			log.Printf( "Process trailer\n")
 			return ( *transform.Trailer)( in_tlr)
