f04676638361038fe80e73718cc38f904678fc14
Rob Anderson <roboprog@yahoo.com>
2013-06-12 07:19:01 -0700

    start a simple platform/language neutral build script

---
+++ make.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -x
+
+# Brute force build for batch library project.
+# Feel free to use make, or ant, or maven
+# as appropriate for the language in which you choose to work.
+# However, this works, even on a Mac, if not on Windows.
+# I'm not writing for Windows.
+
+# This script assumes that Go and Groovy compilers are on the PATH.
+
+( cd examples/go/bin ; \
+	go build ../src/ex1.go )
+
+
+
+
+# vi: ts=4 sw=4
+# *** EOF ***
