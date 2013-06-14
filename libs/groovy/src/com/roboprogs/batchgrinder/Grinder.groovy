799177e2b272f3cd06e8a1dafe8b2c873d55b793
Rob Anderson <roboprog@yahoo.com>
2013-06-14 13:50:17 -0700

    stub for (groovy) library

---
+++ libs/groovy/src/com/roboprogs/batchgrinder/Grinder.groovy
@@ -0,0 +1,16 @@
+package com.roboprogs.batchgrinder
+
+/**
+ * Namespace for library entry point.
+ */
+class Grinder {
+
+	public void run() {
+		println "TODO: run method in library"
+	}
+
+}
+
+
+// vi: ts=4 sw=4
+// *** EOF ****
