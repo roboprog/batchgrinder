99ae4367d2bcc0c23dea064fde2c75b44967ad23
Rob Anderson <roboprog@yahoo.com>
2013-06-26 11:47:36 -0700

    stub for (Groovy) Callbacks interface

--- examples/groovy/src/ex1.groovy
+++ examples/groovy/src/ex1.groovy
@@ -1,9 +1,12 @@
 // simulate a batch job, using framework to pipeline steps
 
 import com.roboprogs.batchgrinder.Grinder
+import com.roboprogs.batchgrinder.grinder.Callbacks
 
 // TODO:  define and implement callback interfaces
-Grinder.run()
+def callbacks = [:]
+
+Grinder.run( callbacks as Callbacks)
 
 
 // vi: ts=4 sw=4
--- libs/groovy/src/com/roboprogs/batchgrinder/Grinder.groovy
+++ libs/groovy/src/com/roboprogs/batchgrinder/Grinder.groovy
@@ -1,26 +1,28 @@
-/*
-    BatchGrinder: batch processing framework
+	/*
+		BatchGrinder: batch processing framework
 
-    Copyright (C) 2013, Robin R Anderson
-    roboprog@yahoo.com
-    PO 1608
-    Shingle Springs, CA 95682
+		Copyright (C) 2013, Robin R Anderson
+		roboprog@yahoo.com
+		PO 1608
+		Shingle Springs, CA 95682
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+		This program is free software: you can redistribute it and/or modify
+		it under the terms of the GNU Lesser General Public License as published by
+		the Free Software Foundation, either version 3 of the License, or
+		(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+		This program is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+		You should have received a copy of the GNU Lesser General Public License
+		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	*/
 
 package com.roboprogs.batchgrinder
+
+import com.roboprogs.batchgrinder.grinder.Callbacks
 
 /**
  * Namespace for library entry point.
@@ -28,7 +30,8 @@ package com.roboprogs.batchgrinder
 class Grinder {
 
 	/** accept the input configuration and start a batch job */
-	public static void run() {
+	public static void run(
+		Callbacks callbacks) {
 		println "TODO: run method in library"
 	}
 
